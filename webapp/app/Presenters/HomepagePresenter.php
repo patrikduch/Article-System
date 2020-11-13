69d8708f4276e3ac48e869b0677b5c594acd7740
patrikduch <duchpatrik@icloud.com>
2020-11-13 15:11:20 +0100

    Basic article listing

--- webapp/app/Presenters/HomepagePresenter.php
+++ webapp/app/Presenters/HomepagePresenter.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace App\Presenters;
 
 use Nette;
@@ -9,15 +7,15 @@ use Nette;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
-
     private $database;
-
 
     public function __construct(Nette\Database\Context $database) {
         $this->database = $database;
     }
 
     public function renderDefault() {
-        $this->template->projectDetail = $this->database->table('ProjectDetail')->limit(1);
+        $this->template->posts = $this->database->table('ARTICLE')
+		->order('PUBLISHED_DATE ASC')
+		->limit(5);
     }
 }
